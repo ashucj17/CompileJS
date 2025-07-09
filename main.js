66f44bf2c2806a5199a8a167f692b52dda75a3ae
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-07-09 19:27:51 +0530

    basic sum

--- main.js
+++ main.js
@@ -3,3 +3,23 @@ function sum (a,b){
 }
 sum(2,3)
 console.log(sum)
+
+
+let val1 = 7;
+let val2 = 10;
+
+function addNum(num1,num2){
+
+    let sum = num1 + num2
+    return sum
+
+}
+
+let result  = addNum(12,14);
+
+console.log(result)
+
+
+
+
+
