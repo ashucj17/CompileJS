e78bf20af9fe6975056189b71037baa8604f4165
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-07-14 23:07:39 +0530

    basic mathemathicla operations

--- modules/execution.js
+++ modules/execution.js
@@ -1,2 +1,16 @@
 let multiply = (a,b)=>console.log(a*b);
 multiply(4,2)
+
+let sqrt = a =>console.log(a*a)
+sqrt(5)
+
+let modulus = (a,b)=>console.log(a%b)
+modulus(16,5)
+
+let devide = (a,b)=> console.log(a/b)
+devide(10,5)
+
+let subtraction = (a,b)=> console.log(a-b)
+subtraction(17,19)
+let addition = (a,b)=> console.log(a+b)
+addition(15,18)
