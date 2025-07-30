f625e8df8198bbf77ce551db9e7d92afb4f6bcf9
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-07-31 01:12:04 +0530

    post and pre increment

--- modules/dataTypeConversions.js
+++ modules/dataTypeConversions.js
@@ -30,5 +30,47 @@ let correctnessInBoolean = Boolean(correctness)
 // console.log(typeof correctnessInBoolean)
 
 let data = NaN
-console.log(data)
-console.log(typeof data)
+// console.log(data)
+// console.log(typeof data)
+
+// console.log(+true)
+// console.log(-true)
+
+// console.log(-'')
+// console.log(+'')
+
+// console.log(+false)
+// console.log(-false)
+
+// console.log('1' + 1)
+// console.log(1 + '1')
+// console.log('1' + '1')
+
+// console.log("2" > 1)
+// console.log("2" >= 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(NaN > 0)
+// console.log(NaN == 0)
+// console.log(NaN >= 0)
+
+let counter = 0
+// counter++; /* post increment operator output = 1 */
+// console.log(counter)
+
+let preCounter = 0;
+++preCounter /* pre increment operator. Output = 1 */
+// console.log(preCounter)
+
+// post increment
+let x = 10
+let y = x++;
+console.log(x , y)
+
+// pre increment
+let a = 10;
+let b = ++a;
+console.log(a , b)
