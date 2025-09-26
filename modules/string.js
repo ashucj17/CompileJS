05175954bec88288a374a06e68839a36a18ee044
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-09-26 19:39:42 +0530

    ${} string interpolation using templet litterals

---
+++ modules/string.js
@@ -0,0 +1,10 @@
+/* ${}  in JS is call tamplet litteral interpolation  or strinng interpolation. It is used inside the tamlate litterals (``) back ticks  for the expression and variables. Mostly used for concatinating string */
+
+let firstName = 'Ashish'
+let lastName = 'Agnihotri'
+
+// let fullName = firstName + lastName //old way
+
+// console.log(firstName + lastName) // old way
+console.log(`hello ${firstName} ${lastName}`)  // using templet litterals
+
