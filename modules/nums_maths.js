d6a23e2e6c9ad1663ff8060c303f2f892cf0a4d8
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-09 17:58:33 +0530

    numbers and maths

---
+++ modules/nums_maths.js
@@ -0,0 +1,35 @@
+// +++++++++++++++Number++++++++++++++++++++++
+
+let score = 100;
+// console.log(score);
+
+let yScore = new Number(100000)
+// console.log(yScore);
+
+// console.log(yScore.valueOf())
+// console.log(yScore.toString())
+// console.log(yScore.toExponential(2))
+// console.log(yScore.toLocaleString('en-GB'))
+
+let myNewScore = 147.5998
+// console.log(myNewScore.toPrecision(4))
+
+
+// ++++++++++ Maths ++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.floor(4.8))
+// console.log(Math.ceil(5.1))
+// console.log(Math.min(44,8,19,33,11))
+// console.log(Math.max(11,9,4,50,-9))
+// console.log(Math.abs(-9))
+
+// console.log(Math.random())
+// console.log(Math.random()+1)
+// console.log(Math.random()*10 + 1)
+// console.log(Math.floor(Math.random()*10)+1)
+
+let min  = 1;
+let max  = 6;
+
+console.log(Math.floor(Math.random() *(max - min + 1)) + min) 
