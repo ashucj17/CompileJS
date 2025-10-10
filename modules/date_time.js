72dc12224a874d0e341245f07b2f031794412f33
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-10 20:01:31 +0530

    Date object

---
+++ modules/date_time.js
@@ -0,0 +1,15 @@
+let today = new Date()
+// console.log(today.toString())
+// console.log(today.toISOString())
+// console.log(today.toUTCString())
+// console.log(today.toDateString())
+// console.log(today.toLocaleString())
+// console.log(typeof today);
+
+let createdDate = new Date(2025,10,25)
+console.log(createdDate.toString())
+// console.log(createdDate.toLocaleString())
+
+
+let customDate = new Date(2025,12,17);
+console.log(customDate.toString())
