1447a4e752f12c87102800308e35b3f126998610
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-11 11:13:21 +0530

    getTime and now()

--- modules/date_time.js
+++ modules/date_time.js
@@ -7,9 +7,15 @@ let today = new Date()
 // console.log(typeof today);
 
 let createdDate = new Date(2025,10,25)
-console.log(createdDate.toString())
+// console.log(createdDate.toString())
 // console.log(createdDate.toLocaleString())
 
 
 let customDate = new Date(2025,12,17);
-console.log(customDate.toString())
+// console.log(customDate.toString())
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(createdDate.getTime())
+console.log(Math.floor(Date.now()/1000))
