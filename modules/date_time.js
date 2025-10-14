e8d29f177a375d3be3743c50c6e5247d8b082965
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-14 13:35:44 +0530

    Customization of toLocalString Method

--- modules/date_time.js
+++ modules/date_time.js
@@ -16,15 +16,26 @@ let customDate = new Date(2025,12,17);
 
 
 let myTimeStamp = Date.now()
-// console.log(myTimeStamp)
+// console.log(myTimeStamp) /* now() property gives the current date */
 // console.log(createdDate.getTime())
-// console.log(Math.floor(Date.now()/1000))
+// console.log(Math.floor(Date.now()/1000)) 
 
 let now = new Date();
 // console.log(now.toISOString()) /* Output:2025-10-13T12:37:46.401Z : where T is the seperator */
-console.log(now.getDate())
-console.log(now.getDay())
-console.log(now.getHours())
-console.log(now.getMinutes())
-console.log(now.getSeconds())
-console.log(now.getFullYear())
+// console.log(now.getDate())
+// console.log(now.getDay())
+// console.log(now.getHours())
+// console.log(now.getMinutes())
+// console.log(now.getSeconds())
+// console.log(now.getFullYear())
+// console.log(now.getMonth() + 1);/*  getMonth() method give the value from 0 to 11 */
+
+/* Customization of toLocalString Method: date.toLocaleString(locale, options)
+  */
+
+console.log(now.toLocaleDateString('en-IN',{hour: '2-digit',
+    minute:'2-digit',
+    second :'2-digit',
+    hour12: true,
+    timeZone: 'Asia/Kolkata'
+}))
