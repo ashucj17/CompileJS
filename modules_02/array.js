d1f60e2f506901116f77d96ba307c4dbe639f795
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-15 20:36:22 +0530

    array basics, array object, .length method

---
+++ modules_02/array.js
@@ -0,0 +1,14 @@
+let myArr = [5,9,7,6,2]
+// console.log(myArr)
+// console.log(myArr[3])
+
+let myArr_2  = new Array("Ashu","Suchit", "Suchit")
+// console.log(myArr_2)
+// console.log(myArr_2.length)
+
+
+/* Array Method */
+
+myArr.push(8)
+console.log(myArr)
+
