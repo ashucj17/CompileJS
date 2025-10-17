65c7a28ea472bc98b21f84f19d78497162471371
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-17 21:05:54 +0530

    join, slice and splice

--- modules_02/array.js
+++ modules_02/array.js
@@ -18,10 +18,29 @@ let myArr_2  = new Array("Ashu","Suchit", "Suchit")
 
 /* Questionable property */
 
-console.log(myArr.includes(2))
-console.log(myArr)
-console.log(myArr.indexOf(2))
-console.log(myArr.includes(4))
+// console.log(myArr.includes(2))
+// console.log(myArr)
+// console.log(myArr.indexOf(2))
+// console.log(myArr.includes(4))
+
+
+/* join method returns a new string by concaenating all the elements, by default it uses the the comma or with the given seperator  */
+
+let newArray = myArr.join()
+// console.log(myArr)
+// console.log(newArray)
+// console.log(myArr_2.join('_'))
+
+/* ssclice and splice */
+
+console.log("Slice",myArr.slice(1,3))
+console.log("OG", myArr)
+console.log('Splice',myArr.splice(1,4))
+console.log("After Splice", myArr)
+
+
+
+
 
 
 
