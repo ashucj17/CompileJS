b5dfed55222d7cfbbce4e1831c965c3c15433487
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-18 21:10:17 +0530

    concat, spread, isArray,from,of

--- modules_02/array.js
+++ modules_02/array.js
@@ -26,19 +26,48 @@ let myArr_2  = new Array("Ashu","Suchit", "Suchit")
 
 /* join method returns a new string by concaenating all the elements, by default it uses the the comma or with the given seperator  */
 
-let newArray = myArr.join()
+// let newArray = myArr.join()
 // console.log(myArr)
 // console.log(newArray)
 // console.log(myArr_2.join('_'))
 
 /* ssclice and splice */
 
-console.log("Slice",myArr.slice(1,3))
-console.log("OG", myArr)
-console.log('Splice',myArr.splice(1,4))
-console.log("After Splice", myArr)
+// console.log("Slice",myArr.slice(1,3))
+// console.log("OG", myArr)
+// console.log('Splice',myArr.splice(1,4))
+// console.log("After Splice", myArr)
 
 
+
+let empName  = ['ashish','rahul','ajay']
+let newEmpName = ['karan','mayak','rohan']
+
+
+// console.log(empName.push(newEmpName))
+// console.log(empName)
+
+let fullStreanth = empName.concat(newEmpName)
+// console.log(fullStreanth)
+
+let totalEmployees = [...empName, ...newEmpName]
+// console.log(totalEmployees)
+
+/* the .push() method add the array at the end in an sigle index and returns the legth of the array.
+Where as the concat() method and spread operator [...]  used to emrge the array and returns the new array */
+
+
+// console.log(Array.isArray("Ashish")) /* isArray() method is used to check whether  the given data is an array or not  */
+// console.log(Array.from('Ashish'))  /* from() method is used to cconvert the data into an array */
+// console.log(Array.from({name: 'Ashish'})) /* here it gives the blank object, We have to specify the key or value */
+
+
+let id = 101
+let name = 'Ashish'
+let designation = 'Frontend Developer'
+
+let myDetails = Array.of(id, name, designation)
+// console.log(myDetails)
 
 
 
