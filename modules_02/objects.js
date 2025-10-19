a49e70dbcf041c4358b4cd7e546927384e1e0400
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-19 21:21:58 +0530

    accessing objects using square brackets

---
+++ modules_02/objects.js
@@ -0,0 +1,17 @@
+// singleton: creating objects usingn singleton - Object.create
+
+// Objects literals
+
+
+let jsUser = {
+    name: 'Ashish',
+    "employee grade": 'SDE3',
+    age: 18,
+    location: 'Kanpur',
+    email: 'ashish@redhat.com',
+    isLoggedIn: true,
+    lastVisited: ['Yesterday','Today','Tomorrow']
+}
+
+console.log(jsUser.name);
+console.log(jsUser["employee grade"]) /* square brackets is used to access keys declared as string */
