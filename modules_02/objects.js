e2985bb75dc94434fd2a55571a56a09297efd459
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-22 10:28:11 +0530

    function in an object and Object.freez

--- modules_02/objects.js
+++ modules_02/objects.js
@@ -18,4 +18,21 @@ let jsUser = {
 
 // console.log(jsUser.name);
 // console.log(jsUser["employee grade"]) /* square brackets is used to access keys declared as string */
-console.log(typeof jsUser[mySymbol])
+// console.log(typeof jsUser[mySymbol])
+
+jsUser.email = "ashish.marcidees.com";
+// console.log(jsUser.email)
+// Object.freeze(jsUser)
+// jsUser.email = "ashish.tatagrp.com";
+// console.log(jsUser)
+
+jsUser.greeting = function(){
+    console.log("Good morning");
+}
+
+jsUser.greeting2 = function(){
+    console.log(`Hello ${this.name} Js Developer`)
+}
+
+console.log(jsUser.greeting())
+console.log(jsUser.greeting2())
