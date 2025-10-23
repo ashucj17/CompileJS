13de0f7f560435a57a66e3edb02d65eea2ed3b85
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-23 17:17:02 +0530

    function argument and parameters and return statement

---
+++ modules_02/functions.js
@@ -0,0 +1,28 @@
+// function myFestivals(){
+//     const family = "Everyone"
+//     console.log(`Happy diwali ${family}`)
+// }
+
+// myFestivals()
+
+// function greating(to){
+//     return `Good evening ${to}`
+// }
+
+// console.log(greating("Ashish"))
+
+// function sum(num1,num2){
+//     console.log(num1 + num2)
+// }
+
+// let result = sum(5,9)
+
+// console.log("Result", result) /* it will display undefined as the function in not returning anything */
+
+function sum(num1,num2){
+    return num1 + num2
+}
+
+let result = sum(5,9)
+
+console.log("Result", result) /* here the function returns addition of num1 and num2  */
