183bbe4a1c3bde70ae105fec379a8b9a418a34f4
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-24 12:33:54 +0530

    objects, array as a parameters of a function

--- modules_02/functions.js
+++ modules_02/functions.js
@@ -25,4 +25,36 @@ function sum(num1,num2){
 
 let result = sum(5,9)
 
-console.log("Result", result) /* here the function returns addition of num1 and num2  */
+// console.log("Result", result) /* here the function returns addition of num1 and num2  */
+
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,500,600,900))
+
+
+let userDetails= {
+    userName: "Ashish",
+    salary: 1250000
+}
+
+function empDetails(rendomUser){
+    console.log(`the employee name is ${rendomUser.userName} and the salary is ${rendomUser.salary}`)
+}
+
+empDetails(userDetails)
+
+// empDetails({
+//     userName: "Rahul",
+//     salary:1080000
+// })
+
+let price = [100,300,500,400]
+
+function cart(bucket){
+    return `the pirce of the product is ${bucket}`
+}
+
+console.log(cart(price[3]))
