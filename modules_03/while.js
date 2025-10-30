12ee792a20d42d92497d0df4652d8cdfe043dae1
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-10-30 21:16:18 +0530

    while and do while

---
+++ modules_03/while.js
@@ -0,0 +1,25 @@
+let hour = 13;
+
+while(hour<=12){
+    // console.log(`The time is ${hour}`)
+    hour++;
+}
+
+do{
+    // console.log(`time is ${hour}`)
+}while(hour<=12)
+
+let heros = ["Flash","Batman","Superman"]
+let arr = 0;
+
+while(arr < heros.length){
+    // console.log(heros[arr])
+    arr++
+}
+
+let vilans= ["Joker","Doom","Null"]
+let v= 3;
+do{
+    console.log(`${vilans[v]} in multiverse`)
+    v++
+}while(v< vilans.length)
