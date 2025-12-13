b700c85ff78ef770a1be8c552006c4272b7e5cec
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-12-13 19:27:10 +0530

    arraw function

--- modules_01/arrowThis.js
+++ modules_01/arrowThis.js
@@ -6,3 +6,17 @@ let user1 = {
 }
 
 user1.great()
+
+let myName = (ageInWords) =>{
+ console.log(ageInWords)
+}
+
+myName("Twenty Nine")
+
+
+let year = 2026
+let newYear = () =>{
+    console.log("About to enter in year", year)
+}
+
+newYear()
